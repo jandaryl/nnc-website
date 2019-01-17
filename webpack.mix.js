f95cb375e92ef8c3da9edae5c2579b199511d888
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2019-01-17 01:58:24 -0500

    Add the bulma and finish the header

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,9 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
-mix.postCss('resources/css/main.css', 'public/css', [
-  tailwindcss('./tailwind.js'),
-]).browserSync('nnc-website.local');
+mix.postCss('resources/css/tailwind.css', 'public/css',
+    [
+        tailwindcss('./tailwind.js'),
+    ])
+    .sass('resources/sass/bulma.scss', 'public/css')
+    .browserSync('nnc-website.local');

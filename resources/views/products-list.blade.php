140ad7acffa2934d4e0c8921f99479ae638594f4
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2019-01-21 09:24:43 -0500

    Update card in product list page

--- resources/views/products-list.blade.php
+++ resources/views/products-list.blade.php
@@ -60,7 +60,7 @@
         </div>
         <div class="inline-flex flex-col items-center h-auto w-1/3 mt-12 rounded-lg shadow-lg font-mono font-medium">
           <a class="p-4 mb-6 text-2xl text-grey-darkest hover:underline" href="{{ route('theophilia-flowerets-farm') }}">Theophilia Flowerets Farm</a>
-          <img class="h-32 w-64 rounded-none mb-4 -mt-2" src="https://wikiki.github.io/images/merry-christmas.jpg" alt="Theophilia Flowerets Farm Logo">
+          <img class="h-32 w-64 rounded-none mb-4 -mt-8" src="https://wikiki.github.io/images/merry-christmas.jpg" alt="Theophilia Flowerets Farm Logo">
           <button class="bg-green hover:bg-green-dark text-white text-1xl font-bold py-2 px-4 h-10 w-24 rounded mb-4">
                      <a class="text-white hover:text-white" href="{{ route('theophilia-flowerets-farm') }}"><i class="fas fa-eye fa-1x mr-2"></i>See</a>
           </button>
